8d061f611f3152c63f487949af161213bd954d92
mister2121 <mrudzki22@gmail.com>
2024-09-03 20:35:10 +0200

    modified css

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -30,28 +30,32 @@ const Form = () => {
 
 	return (
 		<form className='mt-5' onSubmit={handleNewTransaction}>
-			<div className='flex flex-row gap-[5px] mb-[5px]'>
+			<div className='gap-[5px] mb-[5px]'>
+				<input
+					type='text'
+					placeholder='+200'
+					className='w-full border-[2px] border-[#30313d] py-[2px] px-[5px] mb-2 bg-transparent rounded-lg text-[#ddd] '></input>
 				<input
 					type='text'
 					value={name}
 					onChange={(ev) => setName(ev.target.value)}
-					placeholder={"+$200 new Samsung TV"}
-					className='w-full border-[2px] border-[#30313d] py-[2px] px-[5px] bg-transparent rounded-lg'></input>
+					placeholder={"new Samsung TV"}
+					className='w-full border-[2px] border-[#30313d] py-[2px] px-[5px] mb-2 bg-transparent rounded-lg text-[#ddd] '></input>
 				<input
 					type='datetime-local'
 					value={dateTime}
 					onChange={(ev) => setDateTime(ev.target.value)}
-					className='w-full border-[2px] border-[#30313d] py-[2px] px-[5px] bg-transparent rounded-lg text-[#ddd]'></input>
-			</div>
-			<div className='gap-[5px] mb-[5px]'>
+					className='w-full border-[2px] border-[#30313d] py-[2px] px-[5px] mb-2 bg-transparent rounded-lg text-[#ddd] '></input>
 				<input
 					type='text'
 					value={description}
 					onChange={(ev) => setDescription(ev.target.value)}
 					placeholder={"description"}
-					className='w-full border-[2px] border-[#30313d] py-[2px] px-[5px] bg-transparent rounded-lg'></input>
+					className='w-full border-[2px] border-[#30313d] py-[2px] px-[5px] mb-2 bg-transparent rounded-lg text-[#ddd]'></input>
 			</div>
-			<button type='submit' className='w-full p-[5px] rounded-lg bg-[#ddd]'>
+			<button
+				type='submit'
+				className='w-full p-[5px] rounded-lg bg-[#ddd] hover:bg-[#fff]'>
 				Add new transaction
 			</button>
 		</form>
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -7,23 +7,41 @@ const Transactions = ({ transactions }) => {
 				transactions.map((transaction) => (
 					<div
 						key={transaction._id}
-						className='transaction flex justify-between py-[5px] px-0'>
-						<div className='left'>
+						className='transaction flex justify-between py-[5px] px-0 border-[#888] border-t-[1px] first:border-t-0 gap-2'>
+						<div className='left flex-1'>
 							<div className='name text-[1.2rem]'>{transaction.name}</div>
 							<div className='description text-[0.8rem] text-[#888]'>
 								{transaction.description}
 							</div>
 						</div>
-						<div className='right text-right'>
-							<div
-								className={
-									"price " +
-									(transaction.price < 0 ? "text-red-500" : "text-green-500")
-								}>
-								{transaction.price}
+						<div className='right flex items-center gap-2 '>
+							<div className='text-right'>
+								<div
+									className={
+										"price text-[1.2rem] " +
+										(transaction.price < 0 ? "text-red-500" : "text-green-500")
+									}>
+									{transaction.price}
+								</div>
+								<div className='datetime text-[0.9rem] text-[#888]'>
+									2024-08-19 15:45
+								</div>
 							</div>
-							<div className='datetime text-[0.8rem] text-[#888]'>
-								2024-08-19 15:45
+
+							<div>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='size-5 cursor-pointer'>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+									/>
+								</svg>
 							</div>
 						</div>
 					</div>
